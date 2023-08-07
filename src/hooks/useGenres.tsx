3bec74accb2868b0737fba886c7cbaf4f33fb084
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-08-07 07:27:53 +0600

    title fixed

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -1,24 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 
-
 import genres from "../constants/genres";
 
-//npm i -D @types/ms for ts support
-import ms from 'ms'
-import APIClient from "../services/api-client";
+import ms from "ms";
 import Genres from "../entities/Genres";
+import APIClient from "../services/api-client";
 
-const apiClient = new APIClient<Genres>('/genres')
-
+const apiClient = new APIClient<Genres>("/genres");
 
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
 
-    queryFn: apiClient.getData ,
+    queryFn: apiClient.getData,
 
-
-    staleTime: ms('24h'), //24 hours
+    staleTime: ms("24h"), //24 hours
 
     initialData: genres,
   });
